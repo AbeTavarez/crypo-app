b6800627d2eeef3f13381e7cd220a31a93f17e58
Abraham E. Tavarez <abetavarez@icloud.com>
2022-09-09 19:31:55 -0400

    data testing with flatlist

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 import CoinItem from './src/components/CoinItem';
+import Cryptocurrencies from './assets/data/cryptocurrencies.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <CoinItem />
+      <FlatList
+        data={Cryptocurrencies}
+        renderItem={({ item }) => <CoinItem marketCoin={item} />}
+      />
     </View>
   );
 }
