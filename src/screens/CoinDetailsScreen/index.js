e3af6fe38b63c33e3543090664453d692a54fb88
Abraham E. Tavarez <abetavarez@icloud.com>
2022-09-09 20:38:11 -0400

    data import error fixed

---
+++ src/screens/CoinDetailsScreen/index.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const CoinDetailsScreen = () => {
+  return (
+    <View>
+      <Text>Coin details</Text>
+    </View>
+  );
+};
+
+export default CoinDetailsScreen;
