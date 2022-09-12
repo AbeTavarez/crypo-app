75c006cd3d43cbb511c9d8d493bb63bd0d8dea5a
Abraham E. Tavarez <abetavarez@icloud.com>
2022-09-12 18:34:57 -0400

    watchlist components init

---
+++ src/screens/WatchListScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WatchListScreen = () => {
+  return (
+    <View>
+      <Text style={{ color: '#fff' }}>WatchListScreen</Text>
+    </View>
+  );
+};
+
+export default WatchListScreen;
