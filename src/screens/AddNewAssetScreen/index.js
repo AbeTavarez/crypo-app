ddb098ba8ef7949b23ee4dba1c9ab1fde760c38b
Abraham E. Tavarez <abetavarez@icloud.com>
2022-09-27 23:28:36 -0400

    add new assets component created

---
+++ src/screens/AddNewAssetScreen/index.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const AddNewAssetScreen = () => {
+  return (
+    <View>
+      <Text style={{ color: '#fff' }}>Hello</Text>
+    </View>
+  );
+};
+
+export default AddNewAssetScreen;
