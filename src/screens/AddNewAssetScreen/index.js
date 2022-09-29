87c7837b8aa30073bf13c8638c7f6e76d707c0c2
Abraham E. Tavarez <abetavarez@icloud.com>
2022-09-28 21:05:38 -0400

    dropdown init

--- src/screens/AddNewAssetScreen/index.js
+++ src/screens/AddNewAssetScreen/index.js
@@ -1,9 +1,23 @@
 import { Text, View } from 'react-native';
-
+import SearchableDropdown from 'react-native-searchable-dropdown';
+import styles from './styles';
 const AddNewAssetScreen = () => {
   return (
     <View>
-      <Text style={{ color: '#fff' }}>Hello</Text>
+      <SearchableDropdown
+        inItemSelect={(item) => console.log(item)}
+        containerStyle={styles.dropdownContainer}
+        itemStyle={styles.item}
+        itemTextStyle={{ color: '#fff' }}
+        items={[]}
+        resetValue={false}
+        placeholder={'Select a coin...'}
+        placeholderTextColor="#fff"
+        textInputProps={{
+          underlineColorAndroid: 'transparent',
+          style: styles.textInput
+        }}
+      />
     </View>
   );
 };
---
+++ src/screens/AddNewAssetScreen/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  dropdownContainer: {
+    width: '100%',
+    paddingHorizontal: 10,
+    paddingVertical: 20
+  },
+  item: {
+    padding: 10,
+    marginTop: 2,
+    backgroundColor: '#1e1e1e',
+    borderWidth: 1,
+    borderColor: '#444444',
+    borderRadius: 5
+  },
+  textInput: {
+    padding: 12,
+    borderWidth: 1.5,
+    borderColor: '#444444',
+    borderRadius: 5,
+    backgroundColor: '#1e1e1e',
+    color: '#fff'
+  }
+});
+
+export default styles;
