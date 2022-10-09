95239f43dd9ab945818c3dbf6a21c21b20168d21
Abraham E. Tavarez <abetavarez@icloud.com>
2022-10-08 21:22:20 -0400

    if no items on watch list message is displayed

--- src/screens/WatchListScreen/index.js
+++ src/screens/WatchListScreen/index.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
-import { View, Text, FlatList, RefreshControl } from 'react-native';
+import { View, Text, FlatList, RefreshControl, StyleSheet } from 'react-native';
 import { useWatchList } from '../../Contexts/WatchListContext';
 import CoinItem from '../../components/CoinItem';
 import { getWatchListedCoins } from '../../services/apis/cryptoapi';
+import { Ionicons } from '@expo/vector-icons';
 
 const WatchListScreen = () => {
   const { watchListCoinIds } = useWatchList();
@@ -39,21 +40,48 @@ const WatchListScreen = () => {
 
   const transformCoinIds = () => watchListCoinIds.join('%2C');
 
+  console.log('COINS', coins);
+
   return (
     <View>
-      <FlatList
-        data={coins}
-        renderItem={({ item }) => <CoinItem marketCoin={item} />}
-        refreshControl={
-          <RefreshControl
-            refreshing={loading}
-            tintColor="#fff"
-            onRefresh={fetchWatchListedCoins}
-          />
-        }
-      />
+      {coins.length ? (
+        <FlatList
+          data={coins}
+          renderItem={({ item }) => <CoinItem marketCoin={item} />}
+          refreshControl={
+            <RefreshControl
+              refreshing={loading}
+              tintColor="#fff"
+              onRefresh={fetchWatchListedCoins}
+            />
+          }
+        />
+      ) : (
+        <View style={styles.container}>
+          <Text style={styles.addCoins}>Looks pretty empty here...</Text>
+          <Text style={styles.addCoins}>
+            Add coins by tapping the{' '}
+            <Ionicons name={'star'} size={25} color={'#ffbf00'} />
+          </Text>
+        </View>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    // backgroundColor: 'royalblue',
+    borderRadius: 5,
+    marginHorizontal: 15
+  },
+  addCoins: {
+    color: '#fff',
+    alignSelf: 'center',
+    fontSize: 18,
+    marginVertical: 20,
+    fontWeight: 'bold'
+  }
+});
 
 export default WatchListScreen;
